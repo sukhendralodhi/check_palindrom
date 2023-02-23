f391a71e2ccebbf1548870033e0e5e21361175d6
Sukhendra S Lodhi <95135906+sukhendralodhi@users.noreply.github.com>
2023-02-23 12:24:24 +0000

    add one more function in script

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
+// variable declration 
 const inputField = document.getElementById("input");
-const Btn = document.getElementById("checkBtn");
+const btn = document.getElementById("checkBtn");
 let resultHolder = document.querySelector(".result");
+let word = inputField.value;
+
+// function using substring method 
+const subStringMethod = () => {
+  let word = inputField.value;
+  let len = inputField.value.length;
+  let start = word.substring(0, Math.floor(len / 2)).toLowerCase();
+  let end = word.substring(len - Math.floor(len / 2)).toLowerCase();
+
+  let flip = [...end].reverse().join("");
+
+  if (word === "") {
+    inputField.style.borderColor = "red";
+  } else if (start == flip) {
+    // console.log('Palindrom');
+    resultHolder.innerText = `${word} is Palindrom`;
+    inputField.style.borderColor = "purple";
+  } else {
+    // console.log('Not palindrom');
+    resultHolder.innerText = `${word} is not Palindrom`;
+    inputField.style.borderColor = "purple";
+  }
+};
 
 
+// function using inbuild methods 
 const checkPalindrom = () => {
   let palindrom = inputField.value.split("").reverse().join("");
   if (inputField.value == '') {
@@ -17,8 +42,10 @@ const checkPalindrom = () => {
   }
 };
 
-Btn.addEventListener("click", () => {
-  console.log(inputField.value);
-  checkPalindrom();
+// event listener 
+btn.addEventListener("click", () => {
+  // console.log(inputField.value);
+  // checkPalindrom();
+  subStringMethod();
   inputField.value = "";
 });
